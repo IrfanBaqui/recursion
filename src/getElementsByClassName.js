8ad9e21a17f5884507b3ab06e0db4eb882aff648
Irfan Baqui <irfan.baqui@gmail.com>
2015-03-09 21:30:16 -0500

    Implemented the getelementsbyclassname function. Passes specs.

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,19 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-){
-  // your code here
+var getElementsByClassName = function(className) {
+  var result = [];
+
+  var fn = function(el) {
+  	var kids = el.children;
+  	for (var i = 0; i < kids.length; i++) {
+  		if (kids[i].classList.contains(className))
+  			result.push(kids[i]);
+  		if (kids[i].hasChildNodes)
+  			fn(kids[i]);
+  	}
+  }
+  fn(document);
+  return result;
+
 };
